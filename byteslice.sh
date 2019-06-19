1f6ef551be08c484513a8cc7eb6a11b158fd6eae
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2019-06-19 03:19:29 +0200

    Support extensions, convert inputs to target format (tif)

--- byteslice.sh
+++ byteslice.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 # Receive images
-IMAGE_A=$1
-IMAGE_B=$2
+INPUT_A=$1
+INPUT_B=$2
 
 # Receive progress percentage
 PROGRESS=$3
 
 # Receive output file name
 OUTPUT_FILE=$4
+
+# Recieve extension
+EXTENSION=$5
+
+# IMAGE_A=$(echo "$INPUT_A.$EXTENSION")
+# IMAGE_B=$(echo "$INPUT_B.$EXTENSION")
+
+IMAGE_A=$1
+IMAGE_B=$2
+
+# CONF=""
+# convert $INPUT_A $CONF $IMAGE_A
+# convert $INPUT_B $CONF $IMAGE_B
 
 # Obtain number of BYTES in images
 TOTAL_BYTES_A=$(wc -c $IMAGE_A | awk '{print $1}')
@@ -27,20 +40,20 @@ IMAGE_B_BYTES_RESULT=$(echo "$TOTAL_BYTES_B * (100 - $PROGRESS) / 100" | bc -l)
 IMAGE_B_BYTES_FLOAT=$(printf %.2f $(echo "$IMAGE_B_BYTES_RESULT" | bc -l))
 IMAGE_B_BYTES=$(echo "$IMAGE_B_BYTES_FLOAT" | rev | cut -c4- | rev)
 
-echo "Total BYTES in IMAGE_A: 		$TOTAL_BYTES_A"
-echo "Sliced BYTES from IMAGE_A: 		$IMAGE_A_BYTES"
+echo "Total BYTES in $IMAGE_A: 		$TOTAL_BYTES_A"
+echo "Sliced BYTES from $IMAGE_A: 		$IMAGE_A_BYTES"
 
-echo "Total BYTES in IMAGE_B: 		$TOTAL_BYTES_B"
-echo "Skipped BYTES in IMAGE_B: 		$IMAGE_B_SKIP"
-echo "Sliced BYTES from IMAGE_B: 		$IMAGE_B_BYTES"
+echo "Total BYTES in $IMAGE_B: 		$TOTAL_BYTES_B"
+echo "Skipped BYTES in $IMAGE_B: 		$IMAGE_B_SKIP"
+echo "Sliced BYTES from $IMAGE_B: 		$IMAGE_B_BYTES"
 
 # Slice A image
 
 BYTES_TO_COPY_A=$IMAGE_A_BYTES
 
-echo "Copying $BYTES_TO_COPY_A BYTES from IMAGE_A"
+echo "Copying $BYTES_TO_COPY_A BYTES from $IMAGE_A"
 
-OUTPUT_FILE_A=slice-A.tif
+OUTPUT_FILE_A=$(echo ".slice-A-$PROGRESS.$EXTENSION")
 
 cat $IMAGE_A | head -c $BYTES_TO_COPY_A > $OUTPUT_FILE_A
 
@@ -49,21 +62,23 @@ cat $IMAGE_A | head -c $BYTES_TO_COPY_A > $OUTPUT_FILE_A
 BYTES_TO_SKIP_B=$IMAGE_B_SKIP
 BYTES_TO_COPY_B=$IMAGE_B_BYTES
 
-echo "Copying last $BYTES_TO_COPY_B BYTES from IMAGE_B"
+echo "Copying last $BYTES_TO_COPY_B BYTES from $IMAGE_B"
 
-OUTPUT_FILE_B=slice-B.tif
+OUTPUT_FILE_B=$(echo ".slice-B-$PROGRESS.$EXTENSION")
 
 cat $IMAGE_B | tail -c $BYTES_TO_COPY_B > $OUTPUT_FILE_B
 
 # Merge images
 
-echo "Merging images & cleaning up"
+OUTPUT_FINAL=$(echo "stitched-$PROGRESS.$EXTENSION")
 
-cat $OUTPUT_FILE_A $OUTPUT_FILE_B > .STITCH.tif
+echo "Merging images & cleaning up $OUTPUT_FINAL"
 
-convert .STITCH.tif -quality 92 $OUTPUT_FILE
+cat $OUTPUT_FILE_A $OUTPUT_FILE_B > $OUTPUT_FINAL
 
-rm -f .STITCH.TIF
+convert $OUTPUT_FINAL -quality 92 $OUTPUT_FILE
+
+rm -f $OUTPUT_FINAL
 rm -f $OUTPUT_FILE_A
 rm -f $OUTPUT_FILE_B
 
--- render.sh
+++ render.sh
@@ -4,14 +4,18 @@
 IMAGE_A=$1
 IMAGE_B=$2
 OUTPUT_NAME=$3
-STEPS=$4
-STEP_SIZE_FLOAT=$(echo "100 / $STEPS" | bc -l)
+EXTENSION=$4
+
+# STEPS=$5
+# STEP_SIZE_FLOAT=$(echo "100 / $STEPS" | bc -l)
 
 # Slice
+
+echo "My favourite exteions is $EXTENSION"
 
 for i in {1..100}
 do
 	NAME=$(echo "$OUTPUT_NAME-$i.jpg")
 	echo "* Rendering slice $NAME"
-	./byteslice.sh $IMAGE_A $IMAGE_B $i $NAME
+	./byteslice.sh $IMAGE_A $IMAGE_B $i $NAME $EXTENSION
 done
