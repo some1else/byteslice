4e4301f1496318aa0968e71c69d1e8b77ae7e0f0
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2019-07-17 20:12:17 +0200

    Preload new images

--- binprojector/src/components/ImagePreloader/index.js
+++ binprojector/src/components/ImagePreloader/index.js
@@ -8,7 +8,7 @@ const imgStyle = {
 function getImagesFor(imageA, imageB) {
 	const desiredImages = []
 
-	for (let i = 1; i <= 100; i++) {
+	for (let i = 0; i <= 100; i++) {
 		const image = `/mat-lab-3-renders/${imageA}${imageB}.MAT-${i}.MAT.png`
 		desiredImages.push(image)
 	}
@@ -20,20 +20,10 @@ const ImagePreloader = () => {
 	let preloadedImages = []
 
 	const ab = getImagesFor("a", "b")
-	// const	ba = getImagesFor('b', 'a')
-	// const	ac = getImagesFor('a', 'c')
 	const ca = getImagesFor("a", "c")
-	// const	cb = getImagesFor('c', 'b')
 	const bc = getImagesFor("b", "c")
 
-	preloadedImages = preloadedImages.concat(
-		ab,
-		// ba,
-		// ac,
-		ca,
-		// cb,
-		bc
-	)
+	preloadedImages = preloadedImages.concat(ab, ca, bc)
 
 	return (
 		<div>
