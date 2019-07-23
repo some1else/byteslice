985305906c511a369f8fe5791dde22b48681013e
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2019-07-24 00:17:11 +0200

    Test fetch

--- binprojector/src/App.js
+++ binprojector/src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
-import Projector from './components/Projector'
-import ImagePreloader from './components/ImagePreloader'
+import Projector from "./components/Projector"
+import ImagePreloader from "./components/ImagePreloader"
 
-function App() {
-  return (
-    <div className="App">
-      <Projector />
-      <ImagePreloader />
-    </div>
-  );
+class App extends Component {
+	async componentDidMount() {
+		const resp = await fetch("http://localhost:3001/update")
+		const graph = await resp.json()
+		console.log(graph)
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Projector />
+				<ImagePreloader />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
