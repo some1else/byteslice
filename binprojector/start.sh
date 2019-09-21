f37a506b576ee55ca12d6d1a0649949dd228dd3a
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2019-09-22 01:08:19 +0200

    Startup script

---
+++ binprojector/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rm -rf files
+mkdir files
+mkdir files/scraped
+touch files/scraped/.gitkeep
+mkdir files/imported
+touch files/imported/.gitkeep
+mkdir files/sliced
+touch files/sliced/.gitkeep
+
+docker-compose up
