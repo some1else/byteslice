1efca490747b26004d5f9e624cb58e12442fb074
Byte slice <byt@sli.ce>
2019-11-19 02:08:04 +0100

    Kiosk start adaptations
    Signed-off-by: Srđan Prodanović <srdjan.prodanovic@gmail.com>

--- runkiosk.sh
+++ runkiosk.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-google-chrome --kiosk --window-position=0,0 --chrome-frame --disable-first-run-ui --no-default-browser-check --disable-translate --password-store=basic http://localhost:3000
 
-google-chrome --kiosk --window-position=1921,0 --chrome-frame --disable-first-run-ui --no-default-browser-check --disable-translate --password-store=basic http://localhost:3000
+mkdir -p /tmp/kusr1
+mkdir -p /tmp/kusr2
+
+
+
+google-chrome --kiosk --user-data-dir=/tmp/kusr1 --window-position=1921,0 --disable-first-run-ui --no-default-browser-check --disable-translate --password-store=basic http://localhost:3000 &
+
+# google-chrome --user-data-dir=/tmp/kusr1 --kiosk --window-position=0,0 --chrome-frame --disable-first-run-ui --no-default-browser-check --disable-translate --password-store=basic http://localhost:3000 &
+
+
+
+google-chrome --kiosk --user-data-dir=/tmp/kusr2 --window-position=0,0 --disable-first-run-ui --no-default-browser-check --disable-translate --password-store=basic http://localhost:3000 &
+
+# google-chrome --user-data-dir=/tmp/kusr2 --kiosk --window-position=1921,0 --chrome-frame --disable-first-run-ui --no-default-browser-check --disable-translate --password-store=basic http://localhost:3000 &
 
